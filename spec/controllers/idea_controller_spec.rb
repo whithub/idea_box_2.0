8b6b038adda59c9aff01da08df3d367b8f8a82db
Whitney Hiemstra <whitcolorado@gmail.com>
2015-08-24 21:07:21 -0600

    Add idea controller specs

--- spec/controllers/idea_controller_spec.rb
+++ spec/controllers/idea_controller_spec.rb
@@ -2,15 +2,69 @@ require 'rails_helper'
 
 describe 'Ideas', type: :feature do
 
-  it "displays all ideas on root page" do
-
+  before(:each) do
+    FactoryGirl.create(:idea, title: 'First idea title', description: 'Idea #1 Description')
+    FactoryGirl.create(:idea, title: 'Second idea title', description: 'Idea #2 Description')
+    visit root_path
   end
 
-  it "can be created"
+  it "displays all ideas on root page" do
+    expect(page).to have_content('First idea title')
+    expect(page).to have_content('Idea #1 Description')
+    expect(page).to have_content('Second idea title')
+    expect(page).to have_content('Idea #2 Description')
+  end
 
-  it "can be edited"
+  it "can be created" do
+    fill_in "New Idea Title", with: "Brand New Idea!"
+    fill_in "New Idea Description", with: "Description of brand new idea."
+    click_on "Save"
 
-  it "can be deleted"
+    expect(page).to have_content('Brand New Idea!')
+    expect(page).to have_content("Description of brand new idea.")
+    expect(page).to have_content('Second idea title')
+    expect(page).to have_content('Idea #2 Description')
+  end
+
+  it "cannot be created without both an title and description" do
+    fill_in "New Idea Title", with: "Brand New Idea!"
+    click_on "Save"
+
+    expect(page).to have_content("Description cannot be blank.")
+
+    fill_in "New Idea Description", with: "Description of brand new idea."
+
+    expect(page).to have_content('Brand New Idea!')
+    expect(page).to have_content("Description of brand new idea.")
+  end
+
+
+  it "can be edited" do
+    first("Edit").click
+
+    # expect(page). to have_field()
+    expect(page).to have_content('First idea title')
+    expect(page).to have_content('Idea #1 Description')
+
+    fill_in 'First idea title', with: 'Idea Title #1'
+    fill_in 'Idea #1 Description', with: 'Description of Idea #1'
+    click_on "Update"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content('Idea Title #1')
+    expect(page).to have_content('Description of Idea #1')
+    expect(page).to_not have_content('First idea title')
+    expect(page).to_not have_content('Idea #1 Description')
+  end
+
+  it "can be deleted" do
+    first("Delete").click
+
+    expect(page).to_not have_content('First idea title')
+    expect(page).to_not have_content('Idea #1 Description')
+    expect(page).to have_content('Second idea title')
+    expect(page).to have_content('Idea #2 Description')
+  end
 
   it "can have different qualities; lowest is swill, highest is genius"
 
