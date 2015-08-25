0dd771408de8c2ca3f1471d83b059c4604ce9526
Whitney Hiemstra <whitcolorado@gmail.com>
2015-08-24 21:23:39 -0600

    Root path displays all idea titles and descriptions

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,14 @@
 class IdeasController < ApplicationController
 
   def index
-    # @ideas = Idea.all
+    @ideas = Idea.all
+  end
+
+
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :description)
   end
 end
