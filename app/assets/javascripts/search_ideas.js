37dbb2eaca12d2365e0d228d3dfcccc867d2a453
Whitney Hiemstra <whitcolorado@gmail.com>
2015-08-24 22:04:33 -0600

    Able to search ideas by title AND description in real time

--- app/assets/javascripts/search_ideas.js
+++ app/assets/javascripts/search_ideas.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-  var $searchField = $('#idea_search_title')
+  var $searchField = $('#idea_search')
 
   $searchField.on('keyup', function () {
     var searchTerm = this.value.toLowerCase();
     $('.idea').each(function (index, idea) {
-      var title = $(idea).find('h1').text().toLowerCase();
-      var matches = title.indexOf(searchTerm) !== -1;
+      var attribute = $(idea).find('h1, h3').text().toLowerCase(); //|| $(idea).find('h3').text().toLowerCase();
+      var matches = attribute.indexOf(searchTerm) !== -1;
       $(idea).toggle(matches);
     });
   });
