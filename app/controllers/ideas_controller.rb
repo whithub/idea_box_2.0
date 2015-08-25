eed2e3d0f4b7bf2d93f0a07d546c8e490ab6091b
Whitney Hiemstra <whitcolorado@gmail.com>
2015-08-25 15:07:52 -0600

    Able to edit/update an idea

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,17 +1,10 @@
 class IdeasController < ApplicationController
+  before_action :set_idea, only: [:edit, :update, :destroy]
 
   def index
     @ideas = Idea.all
     @idea = Idea.new
   end
-
-  # def show
-  #   @idea = Idea.find(params[:idea_id])
-  # end
-
-  # def new
-  #   @idea = Idea.new
-  # end
 
   def create
     @idea = Idea.new(idea_params)
@@ -34,9 +27,35 @@ class IdeasController < ApplicationController
     # end
   end
 
+  def edit
+  end
+
+  def update
+    if @idea.update_attributes(idea_params)
+      redirect_to root_path, notice: 'Idea was successfully updated.'
+    else
+      flash.now[:errors] = @idea.errors.full_messages.join(", ")
+      render :edit
+    end
+
+    # # respond_to do |format|
+    #   if @idea.update(idea_params)
+    #     format.html { redirect_to root_path, notice: 'Idea was successfully updated.' }
+    #     # format.json { render :show, status: :ok, location: @product }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @product.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
   private
 
   def idea_params
     params.require(:idea).permit(:title, :description)
+  end
+
+  def set_idea
+    @idea = Idea.find(params[:id])
   end
 end
