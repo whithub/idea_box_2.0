c5fbd62e64a6da87d5c2e6c9249c1e0a833c1db5
Whitney Hiemstra <whitcolorado@gmail.com>
2015-08-26 22:33:19 -0600

    Discovered delete action was reloading page, fixed

--- app/assets/javascripts/delete_ideas.js
+++ app/assets/javascripts/delete_ideas.js
@@ -2,6 +2,16 @@ $(document).ready(function () {
   $("a.delete").click(function(event) {
     event.preventDefault();
 
-    $('.idea').remove();
+    var idea = $(this).closest('.idea');
+    var link = $(this);
+    $.ajax({
+      url: link.attr('href'),
+      type: "POST",
+      dataType: "json",
+      data: {"_method":"delete"},
+      complete: function(result) {
+        idea.remove();
+      }
+    });
   });
 });
